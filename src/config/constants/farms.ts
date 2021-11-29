aea0b9e0026c1a0e3efe6feab7f9ac34adfc408c
rockdev <19998916+rockdev101@users.noreply.github.com>
2021-11-29 20:54:05 +0700

    Update LP address

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -11,17 +11,17 @@ const farms: SerializedFarmConfig[] = [
     pid: 0,
     lpSymbol: 'BUSD-SPY',
     lpAddresses: {
-      97: '0xa38F8964F9d9Ad4266494cDD8a5da8e58335f76a',
+      97: '0xc36e31e6489AD518C836b39F1598f5E421CC7741',
       56: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
     },
-    token: serializedTokens.spy,
-    quoteToken: serializedTokens.busd,
+    token: serializedTokens.busd,
+    quoteToken: serializedTokens.spy,
   },
   {
     pid: 1,
     lpSymbol: 'SPY-BNB LP',
     lpAddresses: {
-      97: '0x42fc257C0fD445a504097Fa7fDE11DA4d3550639',
+      97: '0xb50eb4017228820444009a631E1eF8Ff4C9694Ca',
       56: '0x0eD7e52944161450477ee417DE9Cd3a859b14fD0',
     },
     token: serializedTokens.spy,
@@ -31,17 +31,17 @@ const farms: SerializedFarmConfig[] = [
     pid: 2,
     lpSymbol: 'SPY-MSC LP',
     lpAddresses: {
-      97: '0xE322332287766B7f3a9ADfD7f4642F05eb668De8',
+      97: '0x5c029488F79E11b4D818C2871C474D8f21905599',
       56: '',
     },
-    token: serializedTokens.msc,
-    quoteToken: serializedTokens.spy,
+    token: serializedTokens.spy,
+    quoteToken: serializedTokens.msc,
   },
   {
     pid: 3,
     lpSymbol: 'BUSD-BNB LP',
     lpAddresses: {
-      97: '0x7e22Bb130a3Fd9080bdEd79eb107d6716dCD1D3f',
+      97: '0xCe243775e7ec6Dac5702F60a597Ce06344fC60F0',
       56: '0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16',
     },
     token: serializedTokens.busd,
