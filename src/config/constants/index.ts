1823b199d3cc6e0e5d1d7d1148fbb7050a1060f1
rockdev <19998916+rockdev101@users.noreply.github.com>
2021-12-22 08:43:05 -0800

    live factory & router

--- src/config/constants/index.ts
+++ src/config/constants/index.ts
@@ -1,7 +1,7 @@
 import { ChainId, JSBI, Percent, Token } from '@pancakeswap/sdk'
 import { mainnetTokens, testnetTokens } from './tokens'
 
-export const ROUTER_ADDRESS = '0xA36090E981c05b41C88a8d36aF90012235b4e4Eb'
+export const ROUTER_ADDRESS = '0x10ED43C718714eb63d5aA57B78B54704E256024E'
 
 // a list of tokens by chain
 type ChainTokenList = {
